3e6a940a082e4848b2c68d91ac9ab62fca829f11
James Fincher <jamesfincher@Jamess-MacBook-Pro.local>
2023-05-04 00:23:46 -0700

    swtiched railway.json

--- main.py
+++ main.py
@@ -119,5 +119,3 @@ async def create_notebook(input_file: UploadFile = File(...), output: Optional[s
 
     return JSONResponse(content={"result": f"Notebook '{output}' created successfully.", "github_url": github_url})
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
